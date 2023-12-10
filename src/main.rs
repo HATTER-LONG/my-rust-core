2842c88c47c5249148293413e2cd9108ab2c2302
Layton <caolei6767@gmail.com>
2023-12-10 18:38:45 +0800

    update loglevel and makefile to ctrl run&debug

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,69 @@
-#![no_main]
+//! The main module and entrypoint
+//!
+//! The operating system and app also starts in this module. Kernel code starts
+//! executing from `entry.asm`, after which [`rust_main()`] is called to
+//! initialize various pieces of functionality [`clear_bss()`]. (See its source code for
+//! details.)
+//!
+//! We then call [`println!`] to display `Hello, world!`.
+
+#![deny(missing_docs)]
+#![deny(warnings)]
 #![no_std]
+#![no_main]
 #![feature(panic_info_message)]
+
+use core::arch::global_asm;
+use log::*;
 
 #[macro_use]
 mod console;
 mod lang_items;
+mod logging;
 mod sbi;
-use core::arch::global_asm;
 global_asm!(include_str!("entry.asm"));
 
+/// the rust entry point of the kernel
 #[no_mangle]
 pub fn rust_main() -> ! {
+    extern "C" {
+        fn stext(); // begin addr of text segment
+        fn etext(); // end addr of text segment
+        fn srodata(); // start addr of Read-Only data segment
+        fn erodata(); // end addr of Read-Only data ssegment
+        fn sdata(); // start addr of data segment
+        fn edata(); // end addr of data segment
+        fn sbss(); // start addr of BSS segment
+        fn ebss(); // end addr of BSS segment
+        fn boot_stack_lower_bound(); // stack lower bound
+        fn boot_stack_top(); // stack top
+    }
     clear_bss();
-    println!("Hello, world!");
-    panic!("Shutdown machine!");
+    logging::init();
+    println!("[kernel] Hello, world!");
+    trace!(
+        "[kernel] .text [{:#x}, {:#x})",
+        stext as usize,
+        etext as usize
+    );
+
+    debug!(
+        "[kernel] .rodata [{:#x}, {:#x})",
+        srodata as usize, erodata as usize
+    );
+    info!(
+        "[kernel] .data [{:#x}, {:#x})",
+        sdata as usize, edata as usize
+    );
+    warn!(
+        "[kernel] boot_stack top=bottom={:#x}, lower_bound={:#x}",
+        boot_stack_top as usize, boot_stack_lower_bound as usize
+    );
+    error!("[kernel] .bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
+    sbi::shutdown(false)
 }
 
+/// clear Bss segment
 fn clear_bss() {
     extern "C" {
         fn sbss();
